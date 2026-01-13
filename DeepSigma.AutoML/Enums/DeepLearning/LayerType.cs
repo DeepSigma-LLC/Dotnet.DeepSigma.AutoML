8f4073cd96e6739ee6a31202fbb7a58c4c424197
Brenden Perry <brendenperry5@gmail.com>
2026-01-12 21:25:44 -0800

    Add enums for layer types and ensemble techniques
    Introduced LayerType enum for neural network layers and EnsembleLearningTechnique enum for common ensemble learning methods. These additions provide standardized representations for deep learning and machine learning components.

---
+++ DeepSigma.AutoML/Enums/DeepLearning/LayerType.cs
@@ -0,0 +1,21 @@
+﻿
+namespace DeepSigma.AutoML.Enums.DeepLearning;
+
+/// <summary>
+/// Specifies the type of layer in a neural network.
+/// </summary>
+public enum LayerType
+{
+    /// <summary>
+    /// Input layer is the first layer of a neural network that receives the input data and passes it to the subsequent layers for processing.
+    /// </summary>
+    Input,
+    /// <summary>
+    /// Hidden layer is an intermediate layer in a neural network that processes the input data and extracts relevant features before passing them to the output layer.
+    /// </summary>
+    Hidden,
+    /// <summary>
+    /// Output layer is the final layer of a neural network that produces the output predictions or classifications based on the learned features from the previous layers.
+    /// </summary>
+    Output
+}
---
+++ DeepSigma.AutoML/Enums/MachineLearning/EnsembleLearningTechnique.cs
@@ -0,0 +1,27 @@
+﻿
+namespace DeepSigma.AutoML.Enums.MachineLearning;
+
+/// <summary>
+/// Techniques for ensemble learning in machine learning.
+/// </summary>
+public enum EnsembleLearningTechnique
+{
+    /// <summary>
+    /// Bagging (Bootstrap Aggregating) builds many models in parallel on random data subsets to reduce variance, like a diverse team voting (e.g., Random Forest).
+    /// Bagging comes from "Bootstrap AGGregatING," which refers to the process of creating multiple datasets by sampling with replacement (bootstrap) and then aggregating the predictions from models trained on these datasets to improve overall performance and reduce variance.
+    /// </summary>
+    /// <remarks>
+    /// The term "bagging" emphasizes the idea of combining multiple data sets (the "bags") to create a more robust and accurate overall model.
+    /// </remarks>
+    Bagging,
+    /// <summary>
+    /// Boosting builds models sequentially, with each new model correcting errors of the last, to reduce bias, making it powerful but prone to overfitting (e.g., AdaBoost, XGBoost).
+    /// However, a penalty term can be added to the loss function to penalize complex models, helping to prevent overfitting.
+    /// </summary>
+    Boosting,
+    /// <summary>
+    /// Stacking combines multiple models by training a meta-model to learn how to best combine their predictions, leveraging the strengths of each base model for improved overall performance.
+    /// </summary>
+    Stacking,
+
+}
